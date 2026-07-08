6f767bd3d70c4466e929971bb8ef262c5992eefd
GreenCitrus6 <DanielAAad01@gmail.com>
2026-07-08 13:54:44 +0900

    added data import to Card component

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,6 +1,7 @@
 import styles from "./Card.module.css";
 import ResultRecap from "./ResultRecap";
 import ResultSummary from "./ResultSummary";
+import data from "./assets/data.json";
 
 export default function Card() {
     return(
