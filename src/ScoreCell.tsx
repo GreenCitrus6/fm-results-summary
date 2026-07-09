7f6c1bb0d01e2a8dd33742d69dbee73e9dea7a00
GreenCitrus6 <DanielAAad01@gmail.com>
2026-07-09 11:57:18 +0900

    added icons to ScoreCell component

--- src/ScoreCell.tsx
+++ src/ScoreCell.tsx
@@ -7,6 +7,7 @@ interface ComponentProps {
 export default function ScoreCell( { data }: ComponentProps) {
     return(
         <div>
+            <img src={data.icon} />
             {data.category}
             {data.score}/100
         </div>
